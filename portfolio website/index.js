0b594213af3765ff6be000dc55914437af588a28
Bonthu Naga Venkata Ramana <123895150+BNVR@users.noreply.github.com>
2023-05-28 13:23:59 +0530

    Add files via upload

---
+++ portfolio website/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    alert("Submitted");
+    document.getElementById("myname").value="";
+    document.getElementById("email").value="";
+    document.getElementById("tel").value="";
+    document.getElementById("inputs1").value="";
+  }
