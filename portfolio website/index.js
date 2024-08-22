44b941e5d781e0923ffe26cd4f8096812664ef82
Bonthu Naga Venkata Ramana <123895150+BNVR@users.noreply.github.com>
2024-08-23 01:31:59 +0530

    Delete portfolio website directory

--- portfolio website/index.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    alert("Submitted");
-    document.getElementById("myname").value="";
-    document.getElementById("email").value="";
-    document.getElementById("tel").value="";
-    document.getElementById("inputs1").value="";
-  }
